a6b90a178b1be05ad89aa70398eea508e7721275
briannkhata <briannkhata@gmail.com>
2022-08-27 17:44:30 +0200

    House keeping

--- market.py
+++ market.py
@@ -6,11 +6,3 @@ app = Flask(__name__)
 @app.route("/home")
 def home_page():
     return render_template('home.html')
-
-@app.route('/about')
-def about_page():
-    return "<h1>About Page</h1>"
-
-@app.route('/profile/<username>')
-def profile_page(username):
-    return f"<h1>Profile for {username}</h1>"
