3227bffdd0202fc5b2d789c93bb9a76962be3c25
Brian Nkhata <bnkhata@mra.mw>
2022-08-29 08:18:42 +0200

    House keeping

--- market.py
+++ market.py
@@ -6,3 +6,29 @@ app = Flask(__name__)
 @app.route("/home")
 def home_page():
     return render_template('home.html')
+
+@app.route("/login")
+def login_page():
+    return render_template('login.html')
+
+@app.route("/register")
+def register_page():
+    return render_template('register.html')
+
+@app.route("/market")
+def market_page():
+    items = [
+            {
+                'Name':"Brian",
+                'Age':14
+            },
+            {
+                'Name':"Brian2",
+                'Age':14
+            },
+            {
+                'Name':"Brian3",
+                'Age':14
+            }
+        ]
+    return render_template('market.html',items=items)
